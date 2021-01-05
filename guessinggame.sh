859684acb6149ca2accb616472dc058ae86b9dff
anutubu <anubhav@iitk.ac.in>
2021-01-05 17:29:00 +0530

    modified guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-response=1
-nof=0
-while [[ $response -ne $nof ]]
-do
-	echo "how many files are in the current directory"
-	read response
-	nof=$(ls | wc -l)
-	if [[ $nof -eq $response ]]
-	then
-		echo "Congrats your answer is correct"
-	elif [[ $nof -gt $response ]]
-	then	
-		echo "Your answer is low try again"
-	else
-		echo "Your answer is high try again"
-	fi
-done	
+function guess {
+	while [[ $response -ne $nof ]]
+	do
+		if [[ $nof -gt $response ]]
+		then
+			echo "Your guess is too low please enter a number again:"
+			read response
+		elif [[ $nof -lt $response ]]
+		then	
+			echo "Your answer is too high please enter a number again:"
+			read response
+		fi
+	done
+	echo "Congratulations ! Your answer is correct"
+}
 
-		
+echo "Enter the no. of files in the current directory:"
+read response
+nof=$(ls -a | wc -l)
+guess response nof 
